cef79325695ec8105672ba7b6bf6599d55ee25f6
bkenn002 <bkenn002@plattsburgh.edu>
2019-03-06 15:46:20 -0500

    Basic User Management Created, allows a user to input their first and last name as well as their email address. Connection to database made,but have to push and pull information to database.

--- app/src/main/java/com/example/lockeddown/UserActivity.java
+++ app/src/main/java/com/example/lockeddown/UserActivity.java
@@ -2,13 +2,30 @@ package com.example.lockeddown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
+
+    private Button mSubmit;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        mSubmit = (Button) findViewById(R.id.Press_to_Submit);
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
     }
 }
 

b6d8b42fb13d60472ef609cdfb70f00813f46d29
thetrxracer <alexlang121@gmail.com>
2019-03-06 20:47:46 -0500

    Added a button on the main page which pushes a string to the Firebase database. Also messed around with the Gradle files to have the app connecting to the right Google services.

--- app/src/main/java/com/example/lockeddown/MainActivity.java
+++ app/src/main/java/com/example/lockeddown/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.Button;
 
 import com.example.lockeddown.R;
 import com.google.android.gms.common.util.Base64Utils;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private Button mUser_Management;
     private Button mSensors;
+    private Button mSubmit;
+    private DatabaseReference mFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mSensors = (Button) findViewById(R.id.Sensor_Page);
+        //Submit is the button that pushes to Firebase
+        mSubmit = (Button) findViewById(R.id.submit_button);
+        mFirebase = FirebaseDatabase.getInstance().getReference();
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This adds the name to the database
+                //Right now it sets the value to the string in the parenthesis
+                //Next we will pass it a string to set in the database
+                //string
+                mFirebase.child("Project Name").setValue("Locked Down!");
+
+            }
+        });
 
 
     }

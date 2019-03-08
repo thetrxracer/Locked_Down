0a7b898dbcc7b471e72e6c9f554d98c36107cbdf
JoshuaNunez <jnune011@plattsburgh.edu>
2019-03-07 20:11:48 -0500

    class for user

---
+++ app/src/main/java/com/example/lockeddown/User.java
@@ -0,0 +1,54 @@
+package com.example.lockeddown;
+
+
+public class User {
+
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+
+    //required default constructor
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    //Everything from here down might become useful later on.
+    public String getfirstName() {
+         return firstName;
+    }
+
+   public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

05df3f0255259c304dac0c43dd27e9fedc6519ed
JoshuaNunez <jnune011@plattsburgh.edu>
2019-03-14 17:49:12 -0400

    home page opens up first now instead of user management page

--- app/src/main/java/com/example/lockeddown/User.java
+++ app/src/main/java/com/example/lockeddown/User.java
@@ -19,37 +19,37 @@ public class User {
         this.password = password;
     }
 
-    //Everything from here down might become useful later on.
-    public String getfirstName() {
-        return firstName;
-    }
+    //Everything from here down might become useful later on
+    //public String getfirstName() {
+    // return firstName;
+    //}
 
-    public void setfirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    //public void setfirstName(String firstName) {
+    //    this.firstName = firstName;
+    //}
 
-    public String getlastName() {
-        return lastName;
-    }
+    //public String getlastName() {
+    //    return lastName;
+    //}
 
-    public void setlastName(String LastName) {
-        this.lastName = LastName;
-    }
+    //public void setlastName(String LastName) {
+    //    this.lastName = LastName;
+    //}
 
-    public String getEmail() {
-        return email;
-    }
+    //public String getEmail() {
+    //    return email;
+    //}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    //public void setEmail(String email) {
+    //    this.email = email;
+    //}
 
-    public String getPassword() {
-        return password;
-    }
+    //public String getPassword() {
+    //    return password;
+    //}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    //public void setPassword(String password) {
+    //    this.password = password;
+    //}
 }
 
